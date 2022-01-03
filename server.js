cfa1db6a74350605b3a2858860882e04bcc742ef
Kartofel-2834 <kanalori@mail.ru>
2022-01-03 18:18:58 +0300

    I connect my blog's frontend with this server

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const path = require("path")
+const bodyParser = require("body-parser")
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const jsonParser = bodyParser.json()
 
 let port = process.env.PORT;
 
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000
 }
 
 let srcPath = __dirname.split("\\")
@@ -13,11 +18,19 @@ srcPath.length = srcPath.length-1
 srcPath = srcPath.join("\\")
 srcPath = path.join(srcPath, "src")
 
-app.use(express.static(srcPath));
+app.use(cors())
+app.use(urlencodedParser)
+app.use(jsonParser)
+app.use(express.static(srcPath))
 
 
 app.get("/", (req, res)=>{
   res.sendFile( path.join(srcPath, "html", "index.html") )
+})
+
+app.post("/registration", (req, res)=>{
+  console.log(req.body)
+  res.sendStatus(200)
 })
 
 app.listen(port, ()=>{ console.log(`Server working on port ${port}`) })
