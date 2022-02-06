0e00f176adbf25499ba1af5ce04442cf95c975ca
Kartofel-2834 <kanalori@mail.ru>
2022-02-06 12:16:37 +0300

    I add page rendering dependency on user's permissions

--- router/homepage_routes/sendUserInfo.js
+++ router/homepage_routes/sendUserInfo.js
@@ -16,25 +16,54 @@ async function getUserPosts(dbManager, user){
 }
 
 async function getUser(userKeys, dbManager){
-  let user = await dbManager.selectFrom("users", {
-    authkey: userKeys.authKey,
-    tagname: userKeys.tagname
-  })
+  let type = userKeys.authkey ? "owner" : "guest"
 
+  let user = await dbManager.selectFrom("users", userKeys)
   user = user && user.length > 0 ? user[0] : null
+
+  if ( !userKeys.authkey && !user ){
+    return null
+  } else if ( userKeys.authkey && !user ) {
+    type = "guest"
+    user = await dbManager.selectFrom("users", { tagname: userKeys.tagname })
+    user = user && user.length > 0 ? user[0] : null
+  }
+
+  if ( !user || !user.id ){ return null }
 
   user.posts = await getUserPosts(dbManager, user)
 
   let followers = await dbManager.selectFrom("follows", { blog_id: user.id }, null, "COUNT(*)")
   user.followers = followers && followers.length > 0 ? followers[0]["COUNT(*)"] : 0
 
-  return user
+  let follows = await dbManager.selectFrom("follows", { follower_id: user.id }, null, "COUNT(*)")
+  user.follows = follows && follows.length > 0 ? follows[0]["COUNT(*)"] : 0
+
+  delete user.password
+  if ( type == "guest" ){ delete user.authkey }
+
+  return  { type, user }
 }
 
 
 //ROUTE LISTENER
 
 async function sendUserInfo(req, res, dbManager){
+  if ( !req.body.tagname ){ res.status(400).send("Bad request"); return }
+
+  let answer = null
+
+  try {
+    answer = await getUser(req.body, dbManager)
+  } catch(err) {
+    res.status(500).send("Server error"); return
+  }
+
+  if ( !answer ){ res.status(404).send("User not found"); return }
+
+  res.status(200).json(answer)
+
+  /*
   if ( !req.body.authKey || !req.body.tagname ){
     res.status(400).send("Bad request"); return
   }
@@ -43,11 +72,14 @@ async function sendUserInfo(req, res, dbManager){
 
   try {
     user = await getUser(req.body, dbManager)
-  } catch(err) { console.log(err);res.status(500).send("Server error"); return }
+  } catch(err) {
+    res.status(500).send("Server error"); return
+  }
 
   if ( !user ){ res.status(500).send("Server error"); return }
 
   res.status(200).json(user)
+  */
 }
 
 module.exports = sendUserInfo
